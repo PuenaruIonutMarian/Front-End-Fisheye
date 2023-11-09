2171f41464f933504b324737c24d287070cd7bee
Pue90 <43031720+PuenaruIonutMarian@users.noreply.github.com>
2023-11-09 16:44:21 +0100

    first commit

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,45 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const {
+        name,
+        portrait,
+        city,
+        country,
+        tagline,
+        price
+    } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
+        const article = document.createElement('article');
+        const img = document.createElement('img');
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        const h2 = document.createElement('h2');
         h2.textContent = name;
+
+        const divDetails = document.createElement('div');
+        
+
+        const h3 = document.createElement('h3');
+        h3.textContent = `${city}, ${country}`;
+
+        const paragraphTagline = document.createElement('p');
+        paragraphTagline.textContent = tagline;
+
+        const paragraphPrice = document.createElement('p');
+        paragraphPrice.textContent = `${price}€/jour`;
+
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(divDetails);
+        divDetails.appendChild(h3);
+        divDetails.appendChild(paragraphTagline);
+        divDetails.appendChild(paragraphPrice);
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return {
+        name,
+        picture,
+        getUserCardDOM
+    }
 }
